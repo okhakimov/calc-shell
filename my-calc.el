fd85169e2b79b8e06c00fb2b73e77c33a8337da9
okhakimov <okhakimov@gmail.com>
2017-07-08 21:39:15 +0400

    % dvar option added. ii:= is not inserted for '("solve" "now")
    added help for vector creation: index, cvec, map(random,...)

--- my-calc.el
+++ my-calc.el
@@ -4,16 +4,44 @@
 
 ;; Author: Oleg Khakimov <okhakimov@gmail.com>
 ;; Version: 1.0
-;; Package-Requires
+;; Package-Requires: calc
 ;; Keywords: calc
 ;; URL: https://github.com/.../my-calc.el
 
 ;;; Commentary:
 
 ;; my-calc.el is a lightweight wrapper script to help you interact
-;; with calc.  This
-;; package is designed to be unobtrusive and to defer to calc
-
+;; with calc. 
+;; This package is designed to be unobtrusive and to defer to calc
+;;
+;; Write an expression and press F5 to evaluate.
+;;
+;; Examples:
+;;  
+;;  input:                               result:
+;;  ------                               -------  
+;;  
+;;  (1.2+5)*10/2                         ii := (1.200 + 5) 10 / 2 => 31.000
+;;  4^2+2^3+sqrt(16)+8^(1/3)             ii := 4^2 + 2^3 + sqrt(16) + 8^(1/3) => 30.000
+;;  ln(e) + log10(100)                   ii := ln(e) + log10(100) => 3.000
+;;  sin(90) + cos(60) + tan(45)          ii := sin(90) + cos(60) + tan(45) => 2.500
+;;  deg(pi/4)                            ii := deg(pi / 4) => 45.  
+;;  rad(180)                             ii := rad(180) => 3.14159265359  
+;;  arcsin(0.5)                          ii := arcsin(0.5) => 30.  
+;;  
+;;  ;;; assignments:
+;;  a=arccos(0.5)                        a := arccos(0.500) => 60.000
+;;  b=a-30                               b := a - 30 => 30.000
+;;  
+;;  ;; operate on result of previous operation (variable ii):
+;;  18+2                                 ii := 20 
+;;  ii*10                                ii := ii 10 => 200
+;;  
+;;  ;; solving equations:
+;;  f=4x+1=0                             f := 4 x + 1 = 0 => -4 x = 1  
+;;  solve(f,x)                           solve(f, x) => x = -0.25  
+;;  solve(x^2 - 3 x + 1 = 0, x)          solve(x^2 - 3 x + 1 = 0, x) => x = 2.61803398875  
+;;  
 ;;; Code:
 
 
@@ -21,6 +49,9 @@
 (setq my-calc-examples-p nil)
 (setq my-calc-file (locate-library "my-calc"))
 (setq my-calc-dir (file-name-directory my-calc-file))
+(setq my-calc-default-var "ii")
+;; list of expressions that don't assinged to the default var
+(setq my-calc-novar '("solve" "now"))
 
 (defun my-get-curr-line ()
       (buffer-substring-no-properties
@@ -59,9 +90,14 @@
          (newline)
          (previous-line 1)
          (setq expr  (replace-regexp-in-string "\s+$" "" (car (split-string my-calc-org_expr "=>"))))
-         ;add ii:= at the beginning of the line if there is no other assignment
-         (if (not (string-match-p "^ *[_A-Za-z0-9]+ *:?=" expr))
-             (setq expr (replace-regexp-in-string "^" "ii:= " (my-get-curr-line))))
+         ;add ii:= at the beginning of the line if there is no other assignment and it is no in the list of my-calc-novar
+         (if (and (not (string-match-p "^ *[_A-Za-z0-9]+ *:?=" expr))
+		  (not (member 
+                          (replace-regexp-in-string  "^ *" ""
+                            (nth 0 (split-string expr "(")))
+			  my-calc-novar)))
+	       (if my-calc-default-var
+                 (setq expr (replace-regexp-in-string "^" (format "%s:= " my-calc-default-var) (my-get-curr-line)))))
          ;replace = by := 
          (setq expr (replace-regexp-in-string "\\(^ *[_A-Za-z0-9]+ *\\)=" "\\1:= " expr))
          (move-beginning-of-line 1)
@@ -81,7 +117,7 @@
           (next-line 1)
           (my-delete-line-if-empty)
           (move-beginning-of-line 1)
-          (if (my-calc-examples-p)
+          (if my-calc-examples-p
              (progn 
                  (insert (format "%-34s   "  my-calc-org_expr)))))
       (progn 
@@ -99,6 +135,13 @@
              ((equal x "fix") (progn (move-beginning-of-line nil)
                                 (kill-line)
                                 (insert (format "%% [calc-mode: float-format: (fix %s)]" (cadr expr)))
+                                (newline)))
+             ((equal x "dvar") (progn (move-beginning-of-line nil)
+				(setq my-calc-default-var (cadr expr))
+				(if (equal my-calc-default-var "nil")
+                                  (setq my-calc-default-var nil))
+                                (move-end-of-line 1)
+                                (insert (format " : default variable name is set to %s" my-calc-default-var))
                                 (newline)))
              ((equal x "example") (progn (move-beginning-of-line nil)
                                 (setq my-calc-examples-p (cadr expr))                                
@@ -158,13 +201,13 @@ ii*10                                ii := ii 10 => 200
 
 ;; solving equations:
 f=4x+1=0                             f := 4 x + 1 = 0 => -4 x = 1  
-solve(f,x)                           ii := solve(f, x) => x = -0.25  
-solve(x^2 - 3 x + 1 = 0, x)          ii := solve(x^2 - 3 x + 1 = 0, x) => x = 2.61803398875  
+solve(f,x)                           solve(f, x) => x = -0.25  
+solve(x^2 - 3 x + 1 = 0, x)          solve(x^2 - 3 x + 1 = 0, x) => x = 2.61803398875  
 
 ;; system of equations:
 f1=4x+y=0                            f1 := 4 x + y = 0 => 4 x + y = 0  
 f2=x-y=10                            f2 := x - y = 10 => x - y = 10  
-solve([f1,f2],[x,y])                 ii := solve([f1, f2], [x, y]) => [x = 2., y = -8.]  
+solve([f1,f2],[x,y])                 solve([f1, f2], [x, y]) => [x = 2., y = -8.]  
 
 ;; complex numbers. (a,b)= a + i*b. polar notation: (r; arg)
 c1=(3,4)                             c1 := (3, 4) => (3, 4)  
@@ -183,6 +226,10 @@ v_1+v_3                              ii := v_1 + v_3 => 5
 vmean(v)                             ii := vmean(v) => 27  
 vmedian(v)                           ii := vmedian(v) => 3.5  
 vsdev(v)                             ii := vsdev(v) => 48.6826457786  
+v1=index(4)                          v1 := index(4) => [1, 2, 3, 4]
+v2=cvec(100,3)                       v2 := cvec(100, 3) => [100, 100, 100]
+;; generate vector of random numbers
+map(random,cvec(100,3))              ii := map(random, cvec(100, 3)) => [68, 43, 62]
 
 ;; calculus
 f=3x^2 + 2x +1                       f := 3 x^2 + 2 x + 1 => 3 x^2 + 2 x + 1  
@@ -203,6 +250,7 @@ d+10@ 30'                            ii := d + 10@ 30' 0\" => <7:05:51am Tue Jun
 % fix 3 - number of digits in floating point numbers will be 3 (can be set to any number)
 % deg   - angles will be measured in degrees
 % rad   - angles will be measured in radians
+% dvar x   - default variable will be set to x (ii by default). if set to nil, there will be no assignments
 
 Info node `(Calc)'
 Info node `(Calc) Function Index'
